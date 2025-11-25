8dbe115b534dee4a053d9c2886f54791b0dac4ca
ManauTunjae <manau.tunjae@hotmail.com>
2025-11-25 17:23:13 +0100

    Create array information about me.

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   
+  let myInfo = [{
+        firstName: 'Manau',
+        lastName: 'Tunjae',
+        myClass: 'WEBB25',
+        className: 'Fullstack Web development'
+    }];
+
   return (
     <div>
-      
+      <h1>This is a React exercise of WEBB25</h1>
+      <h2>Exercise React Components</h2>
+      {myInfo.map((info) => <Header info={info}/>)}
+      <Main />
     </div>
   )
 }
